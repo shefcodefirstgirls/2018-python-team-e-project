ba8ba61361834214c5874b33c26576998e6cf5ac
Hsin <jedy2468@gmail.com>
2018-03-09 03:25:56 +0000

    basic structure
    basic fomatting, structuring, nav bar and upload function

--- app.py
+++ app.py
@@ -1,12 +1,53 @@
 import os
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,  redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = os.getcwd()+'/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask("my_first_app")
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/share', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return render_template("share.html")
+       
+    
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
 
 @app.route("/")
 def say_hello():
     return render_template("index.html")
+
+@app.route("/posts")
+def to_post():
+    return render_template("post.html")
 
 @app.route("/<name>")
 def say_hello_to(name):
@@ -15,14 +56,15 @@ def say_hello_to(name):
 @app.route("/feedback", methods=["POST"])
 def get_feedback():
     data = request.values
-
     return render_template("feedback.html", form_data=data)
+
 
 """
 This piece of logic checks whether you are running the app locally or on Heroku
 (make an account at https://www.heroku.com/ before the deployment session!). When
 running the app on Heroku, the PORT environment/config variable is pre-populated by
 Heroku to tell our app the correct port to run on.
+
 """
 if "PORT" in os.environ:
     app.run(host="0.0.0.0", port=int(os.environ["PORT"]))
