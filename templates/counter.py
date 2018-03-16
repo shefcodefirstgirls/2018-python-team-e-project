de868c89138e35c52b59cc87032194754f85c5a3
Lukai <lukaizheng7@gmail.com>
2018-03-16 02:56:45 +0000

    Counter doesn't work, but the idea of python writing is there.

---
+++ templates/counter.py
@@ -0,0 +1,17 @@
+
+
+#i=0
+#for i
+#print("do you like it ")
+#like = input(">")
+#if like=="1":
+  # i = i+1
+ #  print(f"{i} people liked it ")
+k = 0
+i = 1
+while i > 0:
+    print("do you like it ")
+    like = input(">") #trigger the bottom of "like"
+    if like=="1":
+        k = k+1
+        print(f"{k} people liked it ")
